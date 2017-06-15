c66f910399046b9183eb2929b2c096c09b8989c3
caojiaju <caojiaju@caojiajudeiMac.local>
2017-06-15 22:09:57 +0800

    1
    1

--- configServer/DSServer/static/js/service1.js
+++ configServer/DSServer/static/js/service1.js
@@ -7,26 +7,55 @@ var serviceCode = 1001;
 
 $(document).ready(function()
 {
+    $.setResultPosition();
     $.search_data();
 });
 
+$(window).resize(function(){
+    $.setResultPosition();
+});
 
 $.extend({
     search_data: function ()
     {
+
         var ckValue = $.cookie('username');
-        if (ckValue != "undefined" && ckValue != ""&& ckValue != null)
+
+        if (ckValue == "undefined" || ckValue == "" || ckValue == null)
         {
             return;
         }
-        var cookieCode = $.cookie('cookie_uuid');
 
+
+        var cookieCode = $.cookie('cookie_uuid');
         $.post("search_data", {username: ckValue, cookiecode: cookieCode,pageindex:pageIndex,pagesize:pageSize,servicecode:serviceCode},
             function (data)
             {
-                // alert(data);
-                $("#resultbody").text( data );
+                // 左侧图片
+                var url = data.name
+
+                var count = data.count;
+
+                for (var i = 0; i < count; i++){
+                    eval("var urlId = data.name" + (i +1 ));
+                    $.addReportImg(urlId);
+                }
+
             },
-            "text");//这里返回的类型有：json,html,xml,text
+            "json");//这里返回的类型有：json,html,xml,text
     },
+
+    addReportImg:function (url) {
+                var newImg = document.createElement("img");
+                newImg.setAttribute("src", url);
+                $("#resultLeft").append(newImg);
+    },
+    setResultPosition:function () {
+
+        $("#resultRight").width($("#result_body_id").width() - $("#resultLeft").width() - 10*4);
+        $("#resultRight").height($(document).height() - $("#top_titile_id").height() - 4*10);
+
+        $("#resultLeft").height($("#resultRight").height());
+
+    }
 });
