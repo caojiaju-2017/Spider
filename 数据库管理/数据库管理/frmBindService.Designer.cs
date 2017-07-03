db723a87d768b0b32cc9a8ac6d5401de4e1d65bc
jiaju_cao <h-sen@foxmail.com>
2017-07-03 20:16:00 +0700

    1

---
+++ 数据库管理/数据库管理/frmBindService.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace 数据库管理
+{
+    partial class frmBindService
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmBindService
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 585);
+            this.Name = "frmBindService";
+            this.Text = "frmBindService";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ 数据库管理/数据库管理/frmBindService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 数据库管理
+{
+    public partial class frmBindService : Form
+    {
+        public frmBindService()
+        {
+            InitializeComponent();
+        }
+    }
+}
