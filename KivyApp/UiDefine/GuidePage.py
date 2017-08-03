2c36d489cc0ce86e68c6f0e18cdd91492dcc1878
caojiaju-2017 <jiaju_cao@hotmail.com>
2017-08-03 08:10:18 +0800

    1
    1

--- KivyApp/UiDefine/GuidePage.py
+++ KivyApp/UiDefine/GuidePage.py
@@ -2,10 +2,12 @@
 #-*- coding:utf-8 -*-
 
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.network.urlrequest import *
 import json
 
 class GuidePage(BoxLayout): #
-
+    def _initialize(self):
+        self.next = self.ids.btn_next.__self__  # same result if I don't use .__self__
     pass
